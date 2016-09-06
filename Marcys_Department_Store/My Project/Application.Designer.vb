69e07aec80249b3c52f3d705eed79e3f96ee7294
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-06 11:04:44 -0500

    Commit

--- Marcys_Department_Store/My Project/Application.Designer.vb
+++ Marcys_Department_Store/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Marcys_Department_Store.Form1
+            Me.MainForm = Global.Marcys_Department_Store.frmMain
         End Sub
     End Class
 End Namespace
